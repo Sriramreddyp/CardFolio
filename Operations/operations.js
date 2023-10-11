d7e396e47d56198feb581fbbe176baa86f325d95
Sriram <sriramreddyp123@gmail.com>
2023-10-12 03:51:23 +0530

    TestAddition routes have been changed to defined input instead of post user assignment due to some other application

--- Operations/operations.js
+++ Operations/operations.js
@@ -17,4 +17,24 @@ function validatePrescription(prescription) {
   else return false;
 }
 
-module.exports = validatePrescription;
+//** function for validating consolidated user obj */
+function validateUser(User) {
+  const UserRegex = /^\d+$/;
+  const passRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_])[a-zA-Z\d\W_]+$/; //! PROBLEM WITH THIS!!
+  let userCheck;
+  let passCheck;
+  let user_id = User.id_card;
+  let password = User.password;
+
+  //? Validating User_id
+  if (user_id.length == 12 && UserRegex.test(user_id)) userCheck = true;
+
+  //? Validating User password
+  if (password.length >= 12) passCheck = true;
+
+  //? Final return Value
+  if (passCheck == true && userCheck == true) return true;
+  else return false;
+}
+
+module.exports = { validatePrescription, validateUser };
--- Routes/testRoutes.js
+++ Routes/testRoutes.js
@@ -3,15 +3,31 @@ const PresModel = require("../Models/Prescription");
 const UserModel = require("../Models/User");
 const TestRouter = express.Router();
 const { body, validationResult } = require("express-validator");
-const validatePrescription = require("../Operations/operations.js");
+const validatingOperations = require("../Operations/operations.js");
 
 //* Consolidated prescription object - For Test purpose will be removed later
 let prescription = {
   user_id: "20BCN7002",
-  doctor_id: "rydh374hd",
+  doctor_id: "rydh374hddadasd",
   diagnosis: [
     { Disease: "Diarrea", medicines: [{ name: "enyon", status: true }] },
   ],
+};
+
+//* Consolidated user object - For Test purpose will be removed later
+let User = {
+  name: "Atreus",
+  id_card: "243456567876",
+  password: "Amman3213dadasdadsa",
+  medical: {
+    Body_weight: 150,
+    Body_height: 200,
+    Blood_Group: "He is God!",
+    Diabetic_status: "He is God!",
+    "Colestrol level": "Just Like his enemies Blood",
+    prescription: [{ prescription_id: "2hd76fsjasd87" }],
+    status: "true",
+  },
 };
 
 TestRouter.get("/", (req, res) => {
@@ -19,51 +35,36 @@ TestRouter.get("/", (req, res) => {
 });
 
 //**Doc addition route to the user Schema */
-TestRouter.post(
-  "/addUser",
-  [
-    //? Validation Parameters
-    body("id_card", "Please enter the Id in required format")
-      .exists()
-      .isLength({ min: 12, max: 12 }),
-    body("password", "Please Enter the Password in required Format")
-      .exists()
-      .isLength({ min: 0, max: 12 })
-      .matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_])[a-zA-Z\d\W_]+$/),
-  ],
-  async (req, res) => {
-    try {
-      //? Input Validation For id_card and password.
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        throw errors;
-      }
-
+TestRouter.post("/addUser", async (req, res) => {
+  try {
+    //? Validation for consolidated user object
+    if (!validatingOperations.validateUser(User)) {
+      console.log("Validation error");
+      res.json({ status: "Not Inserted" });
+    } else {
       //? Creation of schema object
-      const user = new UserModel(req.body);
-
-      //? Query Execution and Handlin
+      const user = new UserModel(User);
+      //? Query Execution and Handling
       await user.save();
       console.log(user);
       res.json({ status: "Sucessfull!!" });
-    } catch (error) {
-      console.log(error);
-      res.json({ status: "Not Inserted" });
     }
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "Not Inserted" });
   }
-);
+});
 
 //**Doc addition route to the prescription Schema */
 TestRouter.post("/presAdd", async (req, res) => {
   try {
     //? Validation for consolidated Prescription object
-    if (!validatePrescription(prescription)) {
+    if (!validatingOperations.validatePrescription(prescription)) {
       console.log("Validation Fault!!");
       res.json({ status: "Not Inserted" });
     } else {
       //? Creation of schema object
       const pres = new PresModel(prescription);
-
       //? Query Execution Handling
       await pres.save();
       console.log(pres);
